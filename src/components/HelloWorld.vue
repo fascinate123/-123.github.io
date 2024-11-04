76fef7dafba1f6492d1ad4783212d31264205dc5
fascinate123 <123352897+fascinate123@users.noreply.github.com>
2024-11-04 19:55:07 +0800

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  
+  <input type="text" v-model.lazy="message">
+  <button @click="ght">按钮</button>
+  <p>{{ message }}</p>
+  <video v-if="message=='蔡徐坤'" src="./抖音2024114-293794.mp3" controls autoplay></video>
+  <video v-if="message=='雷军'" src="./抖音2024114-941774.mp3" controls autoplay></video>
+  <video v-if="message=='薛之谦'" src="./抖音2024114-769974.mp3" controls autoplay></video>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      message:""
+    }
+  }
+  ,methods:{
+    ght(){
+      if(this.message=="薛之谦"){
+        console.log("薛之谦");
+        
+      }
+      else if(this.message=="雷军")
+      {
+        console.log("雷军");
+        
+      }
+    }
+  }
+
+}
+</script>
+
